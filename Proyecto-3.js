5049319a43906c7583b95682c0b51f376cac7485
Agustin_Ibarra <139735608+AgustinIbarra2004@users.noreply.github.com>
2024-06-04 06:00:01 -0300

    Social Media Buttons
    A simple web project featuring buttons that link to various social media profiles, including LinkedIn, GitHub, and Discord.
    Un proyecto web sencillo que incluye botones que enlazan con varios perfiles de redes sociales, incluidos LinkedIn, GitHub y Discord.

---
+++ Proyecto-3.js
@@ -0,0 +1,9 @@
+function abrirLinkedin () {
+    window.open("https://www.linkedin.com/in/agustin-ibarra-4a7861310/");
+}
+function abrirGithub() {
+    window.open("https://github.com/AgustinIbarra2004?tab=overview&from=2024-06-01&to=2024-06-04");
+}
+function abrirDiscord() {
+    window.open("https://discord.gg/nATDFS2h");
+}
